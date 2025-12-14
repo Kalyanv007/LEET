7e36e90b2ae713595c7f9a6988046094acb1937f
Kalyanv007 <168331987+Kalyanv007@users.noreply.github.com>
2025-12-14 23:01:13 +0530

    Time: 87 ms (76.75%) | Memory: 61.9 MB (87.96%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int,int>freq;
+        // unordered_map<int,int>freq;
+        // int count=0;
+        // for(int i=0;i<nums.size();i++){
+        //     int rem=k-nums[i];
+        //     if(freq[rem]>0){
+        //         count++;
+        //         freq[rem]--;
+        //     }
+        //     else freq[nums[i]]++;
+        // }
+        // return count;
+        sort(nums.begin(),nums.end());
+        int sum=0;
+        int i=0,j=nums.size()-1;
         int count=0;
-        for(int i=0;i<nums.size();i++){
-            int rem=k-nums[i];
-            if(freq[rem]>0){
-                count++;
-                freq[rem]--;
+        while(i<j){
+            sum=nums[i]+nums[j];
+            if(sum>k && i<j){
+                j--;
             }
-            else freq[nums[i]]++;
+            else if(sum<k && i<j){
+                i++;
+
+            }
+            else{
+                count++;
+                i++;
+                j--;
+            }
         }
         return count;
     }
