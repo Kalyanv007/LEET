7685b9b9e42f5c8bdcb6064c9b3d87f40acd217e
Kalyanv007 <168331987+Kalyanv007@users.noreply.github.com>
2026-02-23 22:10:51 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (56.82%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -2,7 +2,6 @@ class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         five = 0
         ten = 0
-        twenty = 0
         for i in bills:
             if i == 5:
                 five += 1
