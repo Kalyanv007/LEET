8c562337c90802d7576b65aa965e73ea8d60d760
Kalyanv007 <168331987+Kalyanv007@users.noreply.github.com>
2026-03-26 20:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (7.08%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,16 +1,20 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.res=[]
-        self.combination(candidates,target,[],0)
-        return self.res
-        
-    def combination(self,candidates,target,temp,index):
-        if target==0:
-            self.res.append(temp[:])
-            return
-        for i in range(index,len(candidates)):
-            if candidates[i]<=target:
-                temp.append(candidates[i])
-                self.combination(candidates,target-candidates[i],temp,i)
-                temp.pop()
-        
+    def combinationSum(self, candidates, target):
+        res = []
+        temp = []
+
+        def combinations(index, target):
+            # Base case
+            if target == 0:
+                res.append(temp[:])
+                return
+
+            # Exploration
+            for i in range(index, len(candidates)):
+                if candidates[i] <= target:
+                    temp.append(candidates[i])              # choose
+                    combinations(i, target - candidates[i]) # reuse same index
+                    temp.pop()                              # backtrack
+
+        combinations(0, target)
+        return res
