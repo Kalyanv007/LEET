90e5080003b41df16c45874a2daa115f578fa696
Vasudevan Kalyan <168331987+Kalyanv007@users.noreply.github.com>
2026-06-15 15:23:05 +0530

    Time: 52 ms (37.92%) | Memory: 23 MB (29.54%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -17,7 +17,7 @@ class Solution:
                 if word1[i-1]==word2[j-1]:
                     dp[i][j]=dp[i-1][j-1]
                 else:
-                    dp[i][j]=1+min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])
+                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
                                         #delete,insert,replace in order
         return dp[m][n]
 
