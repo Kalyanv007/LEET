9133b12476223e912fe1e8d7e2dce54048f88717
Vasudevan Kalyan <168331987+Kalyanv007@users.noreply.github.com>
2026-06-18 15:47:59 +0530

    Time: 67 ms (88.00%) | Memory: 19.4 MB (50.55%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,29 +1,36 @@
 class Solution:
-    def atMost(self, nums, k):
+    # def atMost(self, nums, k):
 
-        left = 0
-        right = 0
+    #     left = 0
+    #     right = 0
 
-        freq = {}
-        count = 0
+    #     freq = {}
+    #     count = 0
 
-        while right < len(nums):
+    #     while right < len(nums):
 
-            freq[nums[right]] = freq.get(nums[right], 0) + 1
+    #         freq[nums[right]] = freq.get(nums[right], 0) + 1
 
-            while len(freq) > k:
+    #         while len(freq) > k:
 
-                freq[nums[left]] -= 1
+    #             freq[nums[left]] -= 1
 
-                if freq[nums[left]] == 0:
-                    del freq[nums[left]]
+    #             if freq[nums[left]] == 0:
+    #                 del freq[nums[left]]
 
-                left += 1
+    #             left += 1
 
-            count += (right - left + 1)
+    #         count += (right - left + 1)
 
-            right += 1
+    #         right += 1
 
-        return count
+    #     return count
     def numberOfSubstrings(self, s: str) -> int:
-        return self.atMost(s,3)-self.atMost(s,2)
+        #return self.atMost(s,3)-self.atMost(s,2)
+        count=0
+        lastSeen=[-1,-1,-1]
+        for i in range(len(s)):
+            lastSeen[ord(s[i]) - ord('a')]=i
+            if -1 not in lastSeen:
+                count+=(1+min(lastSeen))
+        return count
